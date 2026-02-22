06b9d4078fa66e856a78db8484cc896f651da1a5
subrat naik <subratnaik2008@gmail.com>
2026-02-22 18:47:00 +0530

    Test Spring project

---
+++ test-spring-project/src/main/java/com/sb/test_spring_project/App.java
@@ -0,0 +1,10 @@
+package com.sb.test_spring_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
