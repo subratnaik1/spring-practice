717862f12dad17f19b18abf05b57a44054ebfc19
subrat naik <subratnaik2008@gmail.com>
2026-02-22 18:50:43 +0530

    Fille updated

--- test-spring-project/src/main/java/com/sb/test_spring_project/App.java
+++ test-spring-project/src/main/java/com/sb/test_spring_project/App.java
@@ -6,5 +6,6 @@ package com.sb.test_spring_project;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Added to check GitHud desktop is working or not");
     }
 }
